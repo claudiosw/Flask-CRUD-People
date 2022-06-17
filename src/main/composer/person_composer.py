18090becea25a94abc1228ef7e529906e99fa62d
Claudio Shigueo Watanabe <claudiosw@gmail.com>
2022-06-16 22:03:18 -0300

    improve: Changed to have one view per method

--- src/main/composer/person_composer.py
+++ src/main/composer/person_composer.py
@@ -1,6 +1,8 @@
-from src.views.person_view import PersonView
+from src.views.registry_person_view import RegistryPersonView
+from src.views.update_person_view import UpdatePersonView
 from src.controllers.registry_person_controller import RegistryPersonController
 from src.views.find_person_view import FindPersonView
+from src.views.delete_person_view import DeletePersonView
 from src.controllers.find_person_controller import FindPersonController
 from src.controllers.update_person_controller import UpdatePersonController
 from src.controllers.delete_person_controller import DeletePersonController
@@ -10,7 +12,7 @@ from src.models.person_repository import PersonRepository
 def person_composer():
     person_repository = PersonRepository()
     person_controller = RegistryPersonController(person_repository)
-    person_view = PersonView(person_controller)
+    person_view = RegistryPersonView(person_controller)
     return person_view
 
 
@@ -24,12 +26,12 @@ def find_person_composer():
 def update_person_composer():
     person_repository = PersonRepository()
     person_controller = UpdatePersonController(person_repository)
-    person_view = PersonView(person_controller)
+    person_view = UpdatePersonView(person_controller)
     return person_view
 
 
 def delete_person_composer():
     person_repository = PersonRepository()
     person_controller = DeletePersonController(person_repository)
-    person_view = FindPersonView(person_controller)
+    person_view = DeletePersonView(person_controller)
     return person_view
--- src/views/delete_person_view.py
+++ src/views/delete_person_view.py
@@ -3,22 +3,16 @@ from .http_types.http_request import HttpRequest
 from .http_types.http_response import HttpResponse
 from .interfaces.views_interface import ViewInterface
 from src.controllers.interface.person_interface import PersonInterface
-from src.validators.person_validator import person_validator
 
 
-class PersonView(ViewInterface):
+class DeletePersonView(ViewInterface):
     def __init__(self, controller: Type[PersonInterface]) -> None:
         self.__controller = controller
 
     def handle(self, http_request: Type[HttpRequest]) -> Type[HttpResponse]:
         try:
-            body = http_request.body
-            person_validator
-            validation_result = person_validator.validate(body)
-            if validation_result:
-                response = self.__controller.run(body)
-            else:
-                return HttpResponse(status_code=400, body={"error": str(person_validator.errors)})
+            query_params = http_request.query_params
+            response = self.__controller.run(query_params)
 
             return HttpResponse(status_code=200, body={"response": response})
         except Exception as exception:
